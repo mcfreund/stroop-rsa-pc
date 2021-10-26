69251946e2e69c11306c3b6cc4fb69b24a27dc0c
mike freund <m.freund@wustl.edu>
2021-10-26 18:31:46 -0500

    functional split_movregs

--- src/evts/split_movregs.R
+++ src/evts/split_movregs.R
@@ -5,6 +5,7 @@
 ## mike freund, 2020-04-04
 ## adapted for ub55 2020-08-20
 
+library(colorout)
 library(here)
 library(dplyr)
 library(magrittr)
@@ -15,18 +16,19 @@ source(here("src", "evts", "write-events_funs.R"))
 dir.analysis <- here("out", "glms")
 
 s <- fread(here("out", "subjlist.csv"))
+s <- s[needs_rerun == FALSE]  ## remove NA rows (why do these exist?)
 
+to.split <- as.data.table(table(s$subj, s$session, s$wave))[N == 1]
+names(to.split)[1:3] <- c("subj", "session", "wave")
+to.split$task <- "Stroop"
 
-to.split <- expand.grid(
-  subj = subjs,
-  task = c("Stroop"),
-  session = c("baseline", "proactive", "reactive"),
-  wave = c("wave1", "wave2"),
-  stringsAsFactors = FALSE
-)
+to.split
 
 movregs <- split.movregs(to.split, dir.to = dir.analysis)
+movregs <- as.data.table(movregs)
 sum(movregs$has.unexpected.nrow)
 sum(movregs$is.missing.dir)
+movregs[has.unexpected.nrow == TRUE]
+movregs[is.missing.dir == TRUE]
 
-
+fwrite(movregs, here("out", "glms", paste0("summary_split_movregs.csv")))
