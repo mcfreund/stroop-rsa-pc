f2a618faccffa41b254278c32fe3c1d4a8cfc39e
mike freund <m.freund@wustl.edu>
2021-11-02 20:45:08 -0500

    Read behavioral data

--- src/4_rsa/1_parcellate_images.r
+++ src/4_rsa/1_parcellate_images.r
@@ -53,10 +53,11 @@ if (interactive()) {
     glms <- "lsall_1rpm"
     tasks <- "Stroop"
     sessions <- "reactive"
-    waves <- "wave1"
+    waves <- c("wave1", "wave2")
     file_prefix <- "STATS"  ## must be length one
     file_suffix <- "REML.func.gii"
-    roiset <- "schaefer"
+    roiset <- "Schaefer2018_control"
+    do_dev_rois <- TRUE
     result_i <- 1
     run_i <- 1
     roi_i <- 1
@@ -67,6 +68,8 @@ if (interactive()) {
 
 }
 
+## read data
+
 atlas <- read_atlas(roiset)
 b <- b[subj %in% subjs & session %in% sessions & wave %in% waves]
 b <- arrange(b, subj, wave, session, run, trial_num)
@@ -76,12 +79,9 @@ b <- arrange(b, subj, wave, session, run, trial_num)
 input <- as.data.table(expand.grid(subj = subjs, task = tasks, wave = waves, session = sessions, glm = glms, run = runs))
 
 
-
 ## execute ----
 
-
 dir_results <- file.path(dir_analysis, input$subj, input$wave, "RESULTS", input$task, paste0(input$session, "_", input$glm))
-
 for (result_i in seq_along(dir_results)) {
     
     ## read giftis
@@ -90,7 +90,13 @@ for (result_i in seq_along(dir_results)) {
 
     files <- file.path(dir_result, paste0(file_prefix, "_", input[result_i]$subj, "_", run_hemis, "_", file_suffix))  ## L then R
     gii <- lapply(files, read_gifti)
-    parcim <- parcellated_image(gii, fold_hemis = run_hemis, atlas = atlas, pattern = "_Coef")  ## construct parcellated image
+    parc_gii <- as_parcellated_list(gii, fold_hemis = run_hemis, atlas = atlas, pattern = "_Coef")  ## construct parcellated data
+
+    ## add trialtype info    
+    
+    b_val <- b[subj == input[result_i]$subj & wave == input[result_i]$wave & session == input[result_i]$session]  ## includes both runs
+    trialtypes <- list(run1 = b_val[run == 1]$item, run2 = b_val[run == 2]$item)
+    parc_gii <- relabel(parc_gii, trialtypes)
 
     ## save in hdf5
     
