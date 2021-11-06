7b602f313d2742b4fd3c792bc396eff6aadda135
mike freund <m.freund@wustl.edu>
2021-11-05 22:41:17 -0500

    Removed DMCC5244053 and DMCC9478705
    - missing RESULTS giftis

--- src/4_rsa/1_parcellate_giftis.sh
+++ src/4_rsa/1_parcellate_giftis.sh
@@ -1,4 +1,7 @@
 
+
+## on first run, had to remove DMCC5820265 and DMCC9478705 from subject list (manually)
+
 waves=("wave1" "wave2")
 sessions=("reactive")
 Rscript ./src/4_rsa/1_parcellate_giftis.r \
@@ -7,3 +10,4 @@ Rscript ./src/4_rsa/1_parcellate_giftis.r \
     --subjlist "out/subjlist_ispc_retest.txt" \
     --waves "wave1 wave2" \
     --sessions "reactive"
+
