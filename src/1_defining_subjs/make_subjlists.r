ac209960c62cd8c177a08e74254fc64036fa43cc
mike freund <m.freund@wustl.edu>
2021-11-09 14:50:34 -0600

    Create all_retest

--- src/1_defining_subjs/make_subjlists.r
+++ src/1_defining_subjs/make_subjlists.r
@@ -8,3 +8,10 @@ s <- fread(here("out", "subjlist.csv"))
 ## development list: reactive test--retest
 ispc_retest <- unique(s[is_ispc_retest == TRUE, "subj"])
 fwrite(ispc_retest, here("out", "subjlist_ispc_retest.txt"), col.names = FALSE)
+
+
+all_retest <- intersect(
+    unique(s[is_ispc_retest == TRUE | is_ispc_retest_cotwin == TRUE, subj]), 
+    unique(s[is_mc_mi_retest == TRUE | is_mc_mi_retest_cotwin == TRUE, subj])
+    )
+fwrite(as.data.table(all_retest), here("out", "subjlist_all_retest.txt"), col.names = FALSE)
