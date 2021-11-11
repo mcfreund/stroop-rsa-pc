4e776401d3c11ee511ae9998dcfe7bcfa6d89fcb
mike freund <m.freund@wustl.edu>
2021-11-11 17:02:51 -0600

    Use <- instead of assign()

--- src/parse_args.r
+++ src/parse_args.r
@@ -5,12 +5,8 @@ roiset <- args$roiset
 subjlist <- args$subjlist
 waves <- strsplit(args$waves, "\\|")[[1]]
 sessions <- strsplit(args$sessions, "\\|")[[1]]
-if ("n_cores" %in% names(args)) args$n_cores <- as.numeric(args$n_cores)  ## optional arg that needs coersion
-
 subjects <- data.table::fread(here("out", paste0("subjlist_", subjlist, ".txt")))[[1]]
 
-lapply(names(args), function(x, l) assign(x, l[[x]]), l = args)  ## assign to variable names in environment
-
-## check some vals
-if (exists("prewh")) stopifnot(prewh %in% c("obsall", "obsresamp"))
-if (exists("measure")) stopifnot(measure %in% c("cveuc", "crcor"))
+if ("n_cores" %in% names(args)) n_cores <- as.numeric(args$n_cores)  ## optional arg that needs coersion
+if ("prewh" %in% names(args)) prewh <- args$prewh
+if ("measure" %in% names(args)) measure <- args$measure
