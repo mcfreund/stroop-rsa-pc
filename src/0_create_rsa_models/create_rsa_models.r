bd3edb9cd62e4c4113e8823d51776e4c698b7d6c
mike freund <m.freund@wustl.edu>
2021-11-15 13:42:03 -0600

    Create create_rsa_models and simulations

---
+++ src/0_create_rsa_models/create_rsa_models.r
@@ -0,0 +1,94 @@
+library(here)
+library(data.table)
+library(magrittr)
+library(httpgd)
+hgd()
+source(here::here("src", "stroop-rsa-pc.R"))
+
+## build label vectors
+
+ttypes <- sort(ttypes)
+target <- gsub("[A-Z]", "", ttypes)
+distractor <- gsub("[a-z]", "", ttypes)
+incongruency <- as.character(target != tolower(distractor))
+
+## build feature vectors (indicators)
+
+ftrs_target <- indicator_matrix(target)
+ftrs_distractor <- indicator_matrix(distractor)
+ftrs_incongruency <- cbind(indicator_matrix(incongruency)[, "TRUE"])
+rownames(ftrs_target) <- ttypes
+rownames(ftrs_distractor) <- ttypes
+rownames(ftrs_incongruency) <- ttypes
+ftrs_conjunction <- ftrs_target * ftrs_distractor
+
+## make models for crcor: cross-run correlation matrices (with downsampling)
+
+models_cor <- c("target", "distractor", "incongruency", "conjunction")
+subsets <- c("all", "bias", "pc50")
+
+l <- list(
+    target = ftrs_target, 
+    distractor = ftrs_distractor, 
+    incongruency = ftrs_incongruency, 
+    conjunction = ftrs_conjunction
+    )
+
+for (ses in sessions) {
+    for (mod in models_cor) {
+        for (sub in subsets) {
+
+            conditions_run1 <- ttypes_by_run[[ses]]$run1
+            conditions_run2 <- ttypes_by_run[[ses]]$run2
+
+            if (sub == "bias") {
+                conditions_run1 <- intersect(ttypes_by_run[[ses]]$run1, ttypes_bias)
+                conditions_run2 <- intersect(ttypes_by_run[[ses]]$run2, ttypes_bias)
+            } else if (sub == "pc50") {
+                conditions_run1 <- intersect(ttypes_by_run[[ses]]$run1, ttypes_pc50)
+                conditions_run2 <- intersect(ttypes_by_run[[ses]]$run2, ttypes_pc50)
+            }
+
+            x <- l[[mod]]
+            X <- tcrossprod(x)[conditions_run1, conditions_run2]
+            image(X)
+            X <- as.data.table(X, keep.rownames = "run1")
+            fwrite(X, here("out", "rsa_models", paste0("model_similarity_", mod, "_", ses, "_", sub, ".csv")))
+            
+
+        }
+    }
+}
+
+
+## make models for cveuc: cross-validated euclidean (no downsampling)
+models_dis <- c("target", "distractor", "incongruency")
+
+for (ses in sessions) {
+    for (mod in models_dis) {
+        for (sub in subsets) {
+
+            conditions_run1 <- ttypes_by_run[[ses]]$run1
+            conditions_run2 <- ttypes_by_run[[ses]]$run2
+
+            if (sub == "bias") {
+                conditions_run1 <- intersect(conditions_run1, ttypes_bias)
+                conditions_run2 <- intersect(conditions_run2, ttypes_bias)
+            } else if (sub == "pc50") {
+                conditions_run1 <- intersect(conditions_run1, ttypes_pc50)
+                conditions_run2 <- intersect(conditions_run2, ttypes_pc50)
+            }
+            
+            x <- l[[mod]]
+            x1 <- x[conditions_run1, ]
+            x2 <- x[conditions_run2, ]
+            X <- cvdist(t(x1), t(x2))
+            X <- X / abs(max(X)) ## scale so max val is 1
+            image(X)
+            dimnames(X) <- list(conditions_run1, conditions_run2)
+            X <- as.data.table(X, keep.rownames = "run1")
+            fwrite(X, here("out", "rsa_models", paste0("model_cvdistance_", mod, "_", ses, "_", sub, ".csv")))
+
+        }
+    }
+}
