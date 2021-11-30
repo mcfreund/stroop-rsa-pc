d969062bb04caaff79dc4d709397e0c29bab165d
mike freund <m.freund@wustl.edu>
2021-11-30 10:49:09 -0600

    Update glmname condition_1rpm

--- src/stroop-rsa-pc.R
+++ src/stroop-rsa-pc.R
@@ -98,7 +98,7 @@ expected_min <- list(
 ## :: analytic info (expected values)
 
 expected <- list(
-    glmname = c("lsall_1rpm", "lss_1rpm", "item_1rpm"),
+    glmname = c("lsall_1rpm", "lss_1rpm", "condition_1rpm"),
     prewh   = c("none", "resid", "obs", "rest"),
     roiset  = 
         c("Schaefer2018Dev", "Schaefer2018Network", "Schaefer2018Parcel", "Glasser2016Network", "Glasser2016Parcel")
