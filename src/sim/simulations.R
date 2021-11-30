a2aad064672ce4250d125a482fd0c3b9faf54a85
mike freund <m.freund@wustl.edu>
2021-11-30 13:08:29 -0600

    Remove unnecessary simulations

--- src/sim/simulations.R
+++ src/sim/simulations.R
@@ -20,14 +20,13 @@ subjlist <- "all_retest"
 subjects <- fread(here("out", paste0("subjlist_", subjlist, ".txt")))[[1]]
 glmname <- "lsall_1rpm"
 wav <- "wave1"
-n_experiments <- 1000  ## number simulations per subject
+n_experiments <- 100  ## number simulations per subject
 n_cores <- 26
 p <- length(ttypes$all)
 Mvec <- create_M(ttypes$all)  ## components of true similarity structure
-a <- setNames(rep(0, ncol(Mvec)), colnames(Mvec))  ## true model weights
 mu <- rep(0, p)  ## true mean activations
 v <- 100  ## number vertices
-n_resamples <- 1E3  ## for crcor
+n_resamples <- 1E2  ## for crcor
 r <- seq(1E-6, 1, 1/4)  ## SNRs
  
 ## read all design matrices into nested list:
@@ -51,36 +50,23 @@ for (session in sessions) {
 
 ## false positive simulations ----
 
-a_baseline <- seq(0, 0.99, 0.9/4)
 params <- expand.grid(
     ttype_subset = "bias", #c("bias", "pc50", "all"),
     ses = "reactive",
     r = r,
-    a_baseline = a_baseline,
     stringsAsFactors = FALSE
 )
-params$a_diagonal <- 1 - a_baseline
-
-
-
+a <- c(1, 0, 0, 0, 0)  ## true model weights
+sigma <- matrix(Mvec %*% a, ncol = p, nrow = p, dimnames = list(ttypes$all, ttypes$all))  ## true geometry
 
 beg <- Sys.time()
-
 dat <- vector("list", nrow(params))
 for (param_i in seq_len(nrow(params))) {
-    # param_i = 1
+    # param_i = 10
     
     ttype_subset <- params$ttype_subset[param_i]
     ses <- params$ses[param_i]
 
-    ##  population parameters
-
-    .r <- params$r[param_i]
-    .a <- c(params$a_baseline[param_i], params$a_diagonal[param_i], 0, 0, 0)    
-    .sigma <- matrix(Mvec %*% .a, ncol = p, nrow = p, dimnames = list(ttypes$all, ttypes$all))  ## true geometry
-
-    ## simulate
-    
     result <- simulate_experiments(
         X_list = X_master[[ses]], 
         Z_list = Z_master[[ses]], 
@@ -88,20 +74,18 @@ for (param_i in seq_len(nrow(params))) {
         Q_dis = Q_dis_master[[ses]][[ttype_subset]],
         .ses = ses,
         .ttype_subset = ttype_subset,
-        .sigma = .sigma,
-        .r = .r
+        .r = params$r[param_i]
     )
     
     ## save
     
     result$session <- ses
     result$ttype_subset <- ttype_subset
-    result$a_baseline <- params$a_baseline[param_i]
     result$r <- .r
 
     dat[[param_i]] <- result
 
 }
 false_positive <- rbindlist(dat)
-
 end <- Sys.time()
+(end - beg)
