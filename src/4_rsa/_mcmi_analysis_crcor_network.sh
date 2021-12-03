3acfdd0e5c835d610b14a33ff0bcf42ae976c05d
mike freund <m.freund@wustl.edu>
2021-12-03 15:53:38 -0600

    Add prewhitening loop

--- src/4_rsa/_mcmi_analysis_crcor_network.sh
+++ src/4_rsa/_mcmi_analysis_crcor_network.sh
@@ -55,3 +55,61 @@ done
 
 
 
+
+## 2. prewhiten coefs
+
+unset prewh
+prewhs=("obsresampbias" "obsresamppc50")
+
+for prewh in ${prewhs[@]}
+do
+    Rscript ./src/4_rsa/prewhiten_coefs.r \
+        --glmname $glmname \
+        --roiset $roiset \
+        --subjlist $subjlist \
+        --waves $wave \
+        --sessions $sessions \
+        --prewh $prewh \
+        --n_cores 25 \
+        --overwrite "FALSE" \
+        --n_resamples 100
+done
+
+
+## 3. estimate distances among prewhitened coefs
+
+for ttype_subset in ${ttype_subsets[@]}
+do
+
+    ## use patterns with prewhitening method that matches ttype_subset:
+
+    if [[ttype_subset = "bias"]]
+    then
+        prewh="obsresampbias"
+    elif [[ttype_subset = "pc50"]]
+        prewh="obsresamppc50"
+    fi
+
+    Rscript ./src/4_rsa/estimate_distances.r \
+        --glmname $glmname \
+        --roiset $roiset \
+        --subjlist $subjlist \
+        --waves $wave \
+        --sessions $sessions \
+        --measure $measure \
+        --prewh $prewh \
+        --ttype_subset $ttype_subset \
+        --n_cores 30 \
+        --n_resamples 10000
+        
+    Rscript ./src/4_rsa/regress_distances.r \
+        --glmname $glmname \
+        --roiset $roiset \
+        --subjlist $subjlist \
+        --waves $wave \
+        --sessions $sessions \
+        --measure $measure \
+        --prewh $prewh \
+        --ttype_subset $ttype_subset
+
+done
