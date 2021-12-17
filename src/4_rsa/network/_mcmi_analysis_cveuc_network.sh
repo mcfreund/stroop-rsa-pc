14a07696b8f65b60587abec8186508ecd49c33a3
mike freund <m.freund@wustl.edu>
2021-12-17 13:58:48 -0600

    Add rest of cveuc

--- src/4_rsa/network/_mcmi_analysis_cveuc_network.sh
+++ src/4_rsa/network/_mcmi_analysis_cveuc_network.sh
@@ -1,12 +1,6 @@
 ## MCMI analysis: baseline wave1, proactive wave 1 ----
 
-
-## 1. initial runthru:
-
-## - no prewhitening
-## - get timing
-
-## on first run, had to remove  from subject list (manually):
+## had to remove  from subject list (manually):
 ## - DMCC9478705
 
 subjlist="mcmi"
@@ -16,38 +10,34 @@ glmname="lsall_1rpm"
 roiset="Schaefer2018Network"
 measure="cveuc"
 ttype_subsets=("bias" "pc50")
+prewhs=("none" "obsbias" "obspc50")
 
 for ttype_subset in ${ttype_subsets[@]}
 do
-
-    ## use patterns with prewhitening method that matches ttype_subset:
-    if [[ $ttype_subset = "bias" ]]
-    then
-        prewh="obsbias"
-    else
-        prewh="obspc50"
-    fi
-    
-    Rscript ./src/4_rsa/estimate_distances.r \
-        --glmname $glmname \
-        --roiset $roiset \
-        --subjlist $subjlist \
-        --waves $wave \
-        --sessions $sessions \
-        --measure $measure \
-        --prewh $prewh \
-        --ttype_subset $ttype_subset \
-        --n_cores 10 \
-        --overwrite "TRUE"
-        
-    Rscript ./src/4_rsa/regress_distances.r \
-        --glmname $glmname \
-        --roiset $roiset \
-        --subjlist $subjlist \
-        --waves $wave \
-        --sessions $sessions \
-        --measure $measure \
-        --prewh $prewh \
-        --ttype_subset $ttype_subset
-
+    echo $ttype_subset ----
+    for prewh in ${prewhs[@]}
+    do
+        echo $prewh ----
+        Rscript ./src/4_rsa/estimate_distances.r \
+            --glmname $glmname \
+            --roiset $roiset \
+            --subjlist $subjlist \
+            --waves $wave \
+            --sessions $sessions \
+            --measure $measure \
+            --prewh $prewh \
+            --ttype_subset $ttype_subset \
+            --n_cores 12 \
+            --overwrite "FALSE"
+            
+        Rscript ./src/4_rsa/regress_distances.r \
+            --glmname $glmname \
+            --roiset $roiset \
+            --subjlist $subjlist \
+            --waves $wave \
+            --sessions $sessions \
+            --measure $measure \
+            --prewh $prewh \
+            --ttype_subset $ttype_subset
+    done
 done
--- src/group_analyses/MCvMI_network_2021-12-16/render.R
+++ src/group_analyses/MCvMI_network_2021-12-16/render.R
@@ -8,14 +8,11 @@ source(here("src", "stroop-rsa-pc.R"))
 src_dir <- "group_analyses/MCvMI_network_2021-12-16"
 ttype_subsets <- c("bias", "pc50")
 prewhs <- c("obsbias", "obspc50", "none")
-measures <- c("cveuc")
+measures <- c("crcor", "cveuc")
 
 params <- expand.grid(ttype_subset = ttype_subsets, prewh = prewhs, measure = measures, stringsAsFactors = FALSE)
 for (i in seq_len(nrow(params))) {
     p <- c(params[i, ])
-    if (p$ttype_subset != gsub("obs", "", p$prewh) && p$measure == "cveuc") next
     render_report(name = "MCMI_Schaefer2016Network", src_dir = src_dir, params = p)
-    #if (p$prewh == "none") {
-        render_report(name = "MIMC_Schaefer2016Network", src_dir = src_dir, params = p)
-    #}
+    render_report(name = "MIMC_Schaefer2016Network", src_dir = src_dir, params = p)
 }
